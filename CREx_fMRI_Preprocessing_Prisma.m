39385a5b0f30aca83bc1983e24d60b7de99070b5
Valérie Chanoine <valerie.chanoine@blri.fr>
2016-09-28 15:40:26 +0200

    Update CREx_fMRI_Preprocessing_Prisma.m

--- CREx_fMRI_Preprocessing_Prisma.m
+++ CREx_fMRI_Preprocessing_Prisma.m
@@ -295,7 +295,7 @@ function DoCoregister(w)
     matlabbatch{1}.spm.spatial.coreg.estimate.source = cellstr(a);
     matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
     matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
-    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.thickness = [4 2];
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
     matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
     matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
    
