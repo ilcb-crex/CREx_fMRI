2b6d48d692f8128a4a5488317ce2a03e702af4fd
Valérie Chanoine <valerie.chanoine@blri.fr>
2016-10-07 10:36:43 +0200

    Update CREx_fMRI_DoRealignFigure.m

--- CREx_fMRI_DoRealignFigure.m
+++ CREx_fMRI_DoRealignFigure.m
@@ -1,27 +1,26 @@
 function CREx_fMRI_DoRealignFigure
     % Do Realign Figure from rp* file on SPM12
-    % Author: Valérie Chanoine, Research Engineer at Brain and Language
-    % Institute (http://www.blri.fr/)
+    % Author: ValÃ©rie Chanoine, Research Engineer at Brain and Language
+    % Research Institute (http://www.blri.fr/)
     % Date: July 25, 2016
 
     clear all;
     set(0,'DefaultFigureColor',[1 1 1]); % Set color background : blank
 
-    st.subjects    = {'01LA', '02CE'}; 
+    st.subjects    = {'01LA', '02CE', '03NC', '04AC', '05GV', '06EC', '07MR', '08NB', '09BK',...
+    '10TL', '11CL', '12SP', '13CC', '14EP','16SM','17NR','18EF', '19AK', '20ET', '21LP', '22SR', '23LG', '24IA', 'pilote2'}; 
+
+
+    st.rootDir     = 'F:\IRM\Intermod2\Data\PREPROC';   
+    st.sessions     = {'func01', 'func02', 'func03', 'func04', 'func05'};    
     
 
-    st.rootDir     = 'F:\IRM\Intermod2\Data\AUDIO';   
-    st.sessions     = {'func01', 'func02', 'func03', 'func04'};    
-
-    
-    for ix=1:numel(st.subjects)  
+	for ix=1:numel(st.subjects)  
         for iS=1:numel(st.sessions)  
-            path = fullfile( st.rootDir,  st.subjects{ix}, 'Functional', st.sessions{iS});
-   
+            path = fullfile( st.rootDir,  st.subjects{ix}, 'Functional', st.sessions{iS});  
+                
             rp = spm_load(spm_select('FPList', path, '^rp.*\.txt$'));
         
-            figure;
-
             subplot(2,1,1);plot(rp(:,1:3));
             set(gca,'xlim',[0 size(rp,1)+1]);
             title ( [st.subjects{ix} ' translation']);
